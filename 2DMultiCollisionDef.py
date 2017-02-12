4bc664e75e093958480d68db9258c225d6569838
EmileTredoux <emiletredoux@gmail.com>
2017-02-12 22:32:49 +0200

    Radian change
    typo

--- 2DMultiCollisionDef.py
+++ 2DMultiCollisionDef.py
@@ -8,7 +8,7 @@ def pos(xi,yi,xvi,yvi,g,t):
       
 def col(x1,y1,x1vi,y1vi,m1,x2,y2,x2vi,y2vi,m2):
     cang = degrees(atan((x1-x2)/(y1-y2)))
-    x1vf = ((((x1vi**2+y1vi**2)**0.5)*cos(radians(90-2*cang))*(m1-m2)+2*m2*((x2vi**2+y2vi**2)**0.5)*cos(radians(degrees(atan(y2vi/x2vi))-cang)))*cos(radians(cang))/(m1+m2)) + ((x1vi**2+y1vi**2)**0.5)*sin(radians(radians(90-2*cang)))*cos(radians(cang)+pi/2)
-    y1vf = ((((x1vi**2+y1vi**2)**0.5)*cos(radians(90-2*cang))*(m1-m2)+2*m2*((x2vi**2+y2vi**2)**0.5)*cos(degrees(radians(atan(y2vi/x2vi))-cang)))*sin(radians(cang))/(m1+m2)) + ((x1vi**2+y1vi**2)**0.5)*sin(radians(radians(90-2*cang)))*sin(radians(cang)+pi/2)
+    x1vf = ((((x1vi**2+y1vi**2)**0.5)*cos(radians(90-2*cang))*(m1-m2)+2*m2*((x2vi**2+y2vi**2)**0.5)*cos(radians(degrees(atan(y2vi/x2vi))-cang)))*cos(radians(cang))/(m1+m2)) + ((x1vi**2+y1vi**2)**0.5)*sin(radians(90-2*cang))*cos(radians(cang)+pi/2)
+    y1vf = ((((x1vi**2+y1vi**2)**0.5)*cos(radians(90-2*cang))*(m1-m2)+2*m2*((x2vi**2+y2vi**2)**0.5)*cos(degrees(radians(atan(y2vi/x2vi))-cang)))*sin(radians(cang))/(m1+m2)) + ((x1vi**2+y1vi**2)**0.5)*sin(radians(90-2*cang))*sin(radians(cang)+pi/2)
     
     
