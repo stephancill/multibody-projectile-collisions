d92b345fb2a46665eb41ff0c7373d06751d914f2
Stephan Cilliers <stephanus.cilliers@gmail.com>
2017-02-13 16:26:29 +0200

    Collision detection: fix collision resultant formula

--- Projectile.js
+++ Projectile.js
@@ -32,60 +32,8 @@ function Projectile (constants, pos={x: 0, y: 0}, size={width:0, height:0}, vi={
     this.updatePosition = function () {
         this.pos.x += this.vel.x*constants.canvasScale;
         this.pos.y -= this.vel.y*constants.canvasScale; // HTML5 Canvas y coordinates ascend from bottom to top, so we reverse is
-        // console.log(projectiles);
-        let self = this
-        // Detect collision
-        projectiles.map(p => {
-            if (p.name !== self.name) {
-                self.comparePositions(p);
-            }
-        });
     }
 
-    this.collision = function () {
-        console.log("collision");
-    }
-
-    this.comparePositions = function (p) {
-        // console.log("Dpos: ", Math.pow(Math.pow((this.pos.x - pos.x), 2)+Math.pow((this.pos.y - pos.y), 2), 0.5), "Radius: ", this.size.width + size.width   );
-        if (Math.pow(Math.pow((this.pos.x - p.pos.x), 2)+Math.pow((this.pos.y - p.pos.y), 2), 0.5) <= this.size.width + p.size.width) {
-            this.collision(p);
-            this.calculateCollision(p)
-        }
-    }
-    this.calculateCollision = function (p) {
-        let x1 = this.pos.x+p.size.width/2;
-        let y1 = this.pos.y+p.size.height/2;
-        let x1vi = this.vel.x;
-        let y1vi = this.vel.y;
-        let m1 = this.mass;
-
-        let x2 = p.pos.x+p.size.width/2;
-        let y2 = p.pos.y+p.size.height/2;
-        let x2vi = p.vel.x;
-        let y2vi = p.vel.y;
-        let m2 = p.mass;
-
-        let cang = 0;
-
-        if (y1 == y2) {
-            cang = 0
-        } else {
-            cang = (180/Math.PI)*(Math.atan((x1-x2)/(y1-y2)))
-        }
-
-        x1vf = ((Math.pow((Math.pow(x1vi,2)+Math.pow(y1vi,2)),0.5)*Math.cos((Math.PI/180)*(90-2*cang))*(m1-m2)+2*m2*(Math.pow(Math.pow(x2vi,2)+Math.pow(y2vi,2)),0.5)*Math.cos((Math.PI/180)*((180/Math.PI)*(Math.atan(y2vi/x2vi))-cang)))*Math.cos((Math.PI/180)*(cang))/(m1+m2)) + (Math.pow(Math.pow(x1vi,2)+Math.pow(y1vi,2)),0.5)*Math.sin((Math.PI/180)*(90-2*cang))*Math.cos((Math.PI/180)*(cang)+Math.PI/2);
-        y1vf = (((Math.pow(Math.pow(x1vi,2)+Math.pow(y1vi,2)),0.5)*Math.cos((Math.PI/180)*(90-2*cang))*(m1-m2)+2*m2*(Math.pow(Math.pow(x2vi,2)+Math.pow(y2vi,2)),0.5)*Math.cos((180/Math.PI)*((Math.PI/180)*(Math.atan(y2vi/x2vi))-cang)))*Math.sin((Math.PI/180)*(cang))/(m1+m2)) + (Math.pow(Math.pow(x1vi,2)+Math.pow(y1vi,2)),0.5)*Math.sin((Math.PI/180)*(90-2*cang))*Math.sin((Math.PI/180)*(cang)+Math.PI/2);
-
-        x2vf = ((Math.pow((Math.pow(x2vi,2)+Math.pow(y2vi,2)),0.5)*Math.cos((Math.PI/180)*(90-2*cang))*(m2-m1)+2*m2*(Math.pow(Math.pow(x1vi,2)+Math.pow(y1vi,2)),0.5)*Math.cos((Math.PI/180)*((180/Math.PI)*(Math.atan(y1vi/x1vi))-cang)))*Math.cos((Math.PI/180)*(cang))/(m2+m1)) + (Math.pow(Math.pow(x2vi,2)+Math.pow(y2vi,2)),0.5)*Math.sin((Math.PI/180)*(90-2*cang))*Math.cos((Math.PI/180)*(cang)+Math.PI/2);
-        y2vf = (((Math.pow(Math.pow(x2vi,2)+Math.pow(y2vi,2)),0.5)*Math.cos((Math.PI/180)*(90-2*cang))*(m2-m1)+2*m2*(Math.pow(Math.pow(x1vi,2)+Math.pow(y1vi,2)),0.5)*Math.cos((180/Math.PI)*((Math.PI/180)*(Math.atan(y1vi/x1vi))-cang)))*Math.sin((Math.PI/180)*(cang))/(m2+m1)) + (Math.pow(Math.pow(x2vi,2)+Math.pow(y2vi,2)),0.5)*Math.sin((Math.PI/180)*(90-2*cang))*Math.sin((Math.PI/180)*(cang)+Math.PI/2);
-
-        console.log(x1vf, y1vf);
-        if (!this.colliding) {
-            this.setVelocity(x1vf*100, y1vf*100);
-            this.colliding = true
-        }
-    }
     // Getters
     this.getPosition = function () {
         return { x: this.pos.x, y: this.pos.y }
@@ -111,19 +59,25 @@ function Projectile (constants, pos={x: 0, y: 0}, size={width:0, height:0}, vi={
         this.vel.y = vy;
     }
 
+    // Rendering
     this.render = function (context) {
         let tmpPos = {x: this.pos.x, y: this.pos.y};
         let tmpTime = this.t;
-        this.updateVelocity();
+        if (!this.colliding) {
+            this.updateVelocity();
+        } else {
+            this.t += 1 * constants.timeScale; // Increment time
+            console.log("I'm colliding");
+        }
         this.updatePosition();
         // console.log("Old velocity", this.vel.x, this.vel.y);
         if (this.pos.y > context.canvas.height-this.size.width) {
             this.pos.y = context.canvas.height-this.size.width;
         }
-        // this.setVelocity((tmpPos.x - this.pos.x)/(tmpTime-this.t)*0.1, (this.pos.y - tmpPos.y)/(tmpTime-this.t)*0.1)
+        this.setVelocity((tmpPos.x - this.pos.x)/(tmpTime-this.t)*0.1, (this.pos.y - tmpPos.y)/(tmpTime-this.t)*0.1)
 
         // Draw
-        context.fillStyle = this.color;
+        context.fillStyle = "white";
         context.fillRect(this.pos.x, this.pos.y, this.size.width, this.size.height)
     }
 }
--- scene.js
+++ scene.js
@@ -7,20 +7,88 @@ const CONSTANTS = {
 let canonBall1 = new Projectile(CONSTANTS);
 canonBall1.setPosition(0, 280);
 canonBall1.setSize(20, 20);
-canonBall1.setVelocity(200,200);
+canonBall1.setVelocity(200,0);
 canonBall1.name = "canonBall1"
 canonBall1.color = "purple"
 
 let canonBall2 = new Projectile(CONSTANTS);
 canonBall2.setPosition(600-20, 280);
 canonBall2.setSize(20, 20);
-canonBall2.setVelocity(-200,200);
+canonBall2.setVelocity(-200,0);
 canonBall2.name = "canonBall2"
 
 let projectiles = [canonBall1, canonBall2];
+let checked = [];
+let collidingProjectiles = [];
 
 let lastFrame = new Date();
 let deltaTime = 0;
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+comparePositions = function (p1, p2) {
+    // console.log("Dpos: ", Math.pow(Math.pow((this.pos.x - pos.x), 2)+Math.pow((this.pos.y - pos.y), 2), 0.5), "Radius: ", this.size.width + size.width   );
+    if (Math.pow(Math.pow((p1.pos.x - p2.pos.x), 2)+Math.pow((p1.pos.y - p2.pos.y), 2), 0.5) <= p1.size.width + p2.size.width) {
+        // calculateCollision(p)
+        return true
+    } else {
+        if (p1.colliding) {
+            p1.colliding = false
+        }
+    }
+}
+calculateCollision = function (p1, p2) {
+    if (!this.colliding) {
+        console.log(p1.vel, p2.vel);
+        let x1 = p1.pos.x+p1.size.width/2;
+        let y1 = p1.pos.y+p1.size.height/2;
+        let x1vi = p1.vel.x;
+        let y1vi = p1.vel.y;
+        let m1 = p1.mass;
+
+        let x2 = p2.pos.x+p2.size.width/2;
+        let y2 = p2.pos.y+p2.size.height/2;
+        let x2vi = p2.vel.x;
+        let y2vi = p2.vel.y;
+        let m2 = p2.mass;
+
+        let cang = 0;
+
+        let ang1 = Math.atan(y1vi/x1vi);
+        let ang2 = Math.atan(y2vi/x2vi);
+        let v1 = Math.pow((Math.pow(x1vi,2)+Math.pow(y1vi,2)),0.5);
+        let v2 = Math.pow((Math.pow(x2vi,2)+Math.pow(y2vi,2)),0.5);
+
+        if (y1 == y2) {
+            cang = 90*Math.PI/180;
+        } else {
+            cang = Math.atan((x1-x2)/(y1-y2));
+        }
+
+        x1vf = (((v1*Math.cos(ang1-cang)*(m1-m2)+2*m2*v2*Math.cos(ang2-cang))/(m1+m2))*Math.cos(cang))+v1*Math.sin(ang1-cang)*Math.cos(cang+Math.PI/2);
+        y1vf = (((v1*Math.cos(ang1-cang)*(m1-m2)+2*m2*v2*Math.cos(ang2-cang))/(m1+m2))*Math.sin(cang))+v1*Math.sin(ang1-cang)*Math.sin(cang+Math.PI/2);
+
+        x2vf = (((v2*Math.cos(ang2-cang)*(m2-m1)+2*m1*v1*Math.cos(ang1-cang))/(m2+m1))*Math.cos(cang))+v2*Math.sin(ang2-cang)*Math.cos(cang+Math.PI/2);
+        y2vf = (((v2*Math.cos(ang2-cang)*(m2-m1)+2*m1*v1*Math.cos(ang1-cang))/(m2+m1))*Math.sin(cang))+v2*Math.sin(ang2-cang)*Math.sin(cang+Math.PI/2);
+
+        console.log("In: \n(obj1)", x1vi, y1vi, " \n(obj2)", x2vi, y2vi);
+        console.log("Out: \n(obj1)", x1vf, y1vf, " \n(obj2)", x2vf, y2vf);
+
+        p1.setVelocity(x1vf, y1vf);
+        p2.setVelocity(x2vf, y2vf);
+
+        p1.colliding = true;
+        p2.colliding = true;
+    }
+}
 function update() {
     if (!stop) {
         deltaTime = new Date()-lastFrame;
@@ -28,7 +96,27 @@ function update() {
         updateLogging()
         cc.fillStyle = "black";
         cc.fillRect(0, 0, c.width, c.height);
+
+        collidingProjectiles.map(pair => {
+            pair.map(p => {
+                p.colliding = false;
+            })
+        })
+        for (var i = 0; i < projectiles.length; i++) {
+            projectiles.map(p => {
+                if (projectiles[i].name !== p.name && !checked.contains(projectiles[i])) {
+                    if (comparePositions(projectiles[i], p)) {
+                        console.log("Collision detected");
+                        calculateCollision(projectiles[i], p);
+                        collidingProjectiles.push([projectiles[i], p])
+                        checked.push(...[projectiles[i], p]);
+                    }
+                }
+
+            })
+        }
         projectiles.map(p=> {
+            // console.log(p.vel);
             p.render(cc);
         });
     }
