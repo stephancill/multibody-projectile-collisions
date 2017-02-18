edeb3e654b3dd01c206524157401b113fd57808e
Stephan Cilliers <stephanus.cilliers@gmail.com>
2017-02-18 20:14:53 +0200

    Removed linear equation solver

--- analytical/Equations.js
+++ analytical/Equations.js
@@ -1,28 +1,22 @@
 function Equations() {
-    this.solveQuadratic = function(a, b, c) {
+    this.solveQuadratic = function({a, b, c}) {
         /*
+        Quadratic equation
         returns positive x or null
         */
+
         let descriminant = Math.pow(Math.pow(b, 2) - 4*a*c, 0.5);
         if (descriminant >= 0) {
             let x1 = (-b + descriminant)/(2*a);
             let x2 = (-b - descriminant)/(2*a);
             if (x1 > 0) {
                 return x1;
-            } else {
+            } else if (x2 > 0) {
                 return x2;
+            } else {
+                return 0;
             }
             console.log(x1, x2);
-        } else {
-            return null;
-        }
-    }
-
-    this.solveLinear = function(y, m, c) {
-        if (m === 0) {return null};
-        let x = (y-c) / m;
-        if (x > 0) {
-            return x;
         } else {
             return null;
         }
